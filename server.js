176be022b2d7bb853e89e7b5692676db5ad781bb
Vijendra Kumar <vijendrakumarram@gmail.com>
2019-11-26 15:19:25 -0800

    modify server file

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("test-app");
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(process.env.PORT || 8080, function(){
+console.log('listening on: 8080');
+});
+
+console.log('Yeh....server is running...');
